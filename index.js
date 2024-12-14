119e409b1320b66d53aaa0c7d4c12110ce64a049
darrrrel009 <darell.lundy009@gmail.com>
2024-12-14 21:19:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();  // Loads variables from .env file
-const Web3 = require('web3');  // Correct Web3 import
+const { Web3 } = require('web3');  // Correct Web3 import for version 4.x
 
 // Connect to Ethereum network (via Infura or another provider)
 const web3 = new Web3(process.env.INFURA_URL);  // Directly pass the Infura URL here
